4a1ceb12a3267597cad8ca5b25457f90b1c44482
Puleeno Nguyen <puleeno@gmail.com>
2020-09-22 17:30:52 +0700

    Support remove add to cart button

--- src/Plugin/WooCommerce.php
+++ src/Plugin/WooCommerce.php
@@ -37,16 +37,23 @@ class WooCommerce implements ShopPlugin
 
     public function initHooks()
     {
+        // Make theme support WooCommerce
         add_theme_support('woocommerce');
+
+        // Register WooCommerce widgets
         add_action('widgets_init', array($this, 'registerShopSidebars'));
 
-        add_action('jankx_template_page_single_product', array($this, 'renderProductContent'));
         add_action('jankx_template_build_site_layout', array($this, 'customShopLayout'));
-
+        add_action('jankx_template_page_single_product', array($this, 'renderProductContent'));
         add_action('jankx_template_pre_get_current_site_layout', array($this, 'changeCurrentSiteLayout'));
 
+        // Custom WooCommercce templates
         add_filter('wc_get_template', array($this, 'changeWooCommerceTemplates'), 10, 5);
+
+        // Make WooCommerce is global
         add_filter('body_class', array($this, 'addWoocommerceCSSBodyClass'));
+
+        add_action('template_redirect', array($this, 'customWooCommerceElements'));
     }
 
     public function registerShopSidebars()
@@ -83,6 +90,9 @@ class WooCommerce implements ShopPlugin
     public function customShopLayout($layoutLoader)
     {
         if (is_woocommerce()) {
+            remove_action('woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
+            remove_action('woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
+
             remove_action('jankx_template_after_main_content', 'get_sidebar', 35);
             remove_action('jankx_template_after_main_content', array($layoutLoader, 'loadSecondarySidebar'), 45);
 
@@ -148,5 +158,12 @@ class WooCommerce implements ShopPlugin
             $classes[] = 'woocommerce';
         }
         return $classes;
+    }
+
+    public function customWooCommerceElements()
+    {
+        if (apply_filters('jankx_ecommerce_woocommerce_dislabe_loop_add_to_cart', false)) {
+            remove_action('woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart');
+        }
     }
 }
