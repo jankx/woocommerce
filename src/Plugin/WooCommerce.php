8171150645847a57a708053f93088c86da7cdc19
Puleeno Nguyen <puleeno@gmail.com>
2020-09-22 17:41:52 +0700

    Support product summary sidebar

--- src/Plugin/WooCommerce.php
+++ src/Plugin/WooCommerce.php
@@ -73,6 +73,22 @@ class WooCommerce implements ShopPlugin
             'jankx_ecommerce_woocommerce_sidebar_args',
             $shopSidebarArgs
         ));
+
+        $shopSidebarArgs = array(
+            'id' => 'product_summary',
+            'name' => __('Product Summary Sidebar', 'jankx'),
+            'description' => __('The widgets of the product summary will be show at here', 'jankx'),
+            'before_widget' => '<section id="%1$s" class="widget jankx-widget %2$s">',
+            'after_widget' => '</section>',
+            'before_title' => '<h3 class="jankx-title widget-title">',
+            'after_title' => '</h3>',
+        );
+
+        // Register shop sidebar
+        register_sidebar(apply_filters(
+            'jankx_ecommerce_woocommerce_product_summary_sidebar_args',
+            $shopSidebarArgs
+        ));
     }
 
     protected function checkSidebarIsActive()
