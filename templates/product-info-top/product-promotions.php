7b8b787a06946a188feb598e7e1ee165f371d341
Puleeno Nguyen <puleeno@gmail.com>
2020-11-13 11:52:19 +0700

    Added promotions for product

---
+++ templates/product-info-top/product-promotions.php
@@ -0,0 +1,21 @@
+<div class="box-km mb-3">
+    <h6 class="font-weight-600 mb-3">Khuyến mại</h6>
+    <label class="label-checkbox">
+        Miễn phí công TƯ VẤN, KHẢO SÁT tại nhà
+        <a
+            href="https://janhome.vn/vui-tet-thieu-nhi-1-6-hoan-hi-nhan-qua-khuyen-mai-lon-cua-janhome-he-thong-ban-le-vat-lieu-hoan-thien-tai-kho.html">
+            - (<span class="extra-link-promotion">Xem chi tiết</span>)</a>
+        <input type="checkbox" class="choose_promotion" checked="checked" data-promotion-id="2" data-is-discount="0"
+            data-type="free-setup" data-value="5">
+        <span class="checkmark"></span>
+    </label>
+    <label class="label-checkbox">
+        Đơn hàng trên 15tr hoặc trên 70m2 thì miễn phí vận chuyển bán kính 15km
+        <a
+            href="https://janhome.vn/vui-tet-thieu-nhi-1-6-hoan-hi-nhan-qua-khuyen-mai-lon-cua-janhome-he-thong-ban-le-vat-lieu-hoan-thien-tai-kho.html">
+            - (<span class="extra-link-promotion">Xem chi tiết</span>)</a>
+        <input type="checkbox" class="choose_promotion" checked="checked" data-promotion-id="8" data-is-discount="0"
+            data-type="free-setup" data-value="0">
+        <span class="checkmark"></span>
+    </label>
+</div>
