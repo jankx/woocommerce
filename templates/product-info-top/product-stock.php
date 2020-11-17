8bfd388ad349a64bed9ced18cde184f32f73587b
Puleeno Nguyen <puleeno@gmail.com>
2020-11-17 21:30:31 +0700

    Change templates single product

---
+++ templates/product-info-top/product-stock.php
@@ -0,0 +1,9 @@
+<div class="quickdelivery">
+    <div class="deliverytime">
+        <span class="con_hang">Còn hàng</span>
+            <a href="javascript:" class="onoffdelibox">
+                <i class="mdi mdi-map-marker-radius"></i> Chọn địa chỉ nhận hàng
+            </a>
+            để biết thời gian giao.
+    </div>
+</div>
