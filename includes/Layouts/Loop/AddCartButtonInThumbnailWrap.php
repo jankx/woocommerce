5c99486ea836c291fbc78efd9c196c31a84c0be4
Puleeno Nguyen <puleeno@gmail.com>
2025-03-05 15:24:17 +0700

    Support new loop layout `add to cart button in thumbnail wrapper`

--- includes/Layouts/Loop/AddCartButtonInThumbnailWrap.php
+++ includes/Layouts/Loop/AddCartButtonInThumbnailWrap.php
@@ -17,18 +17,65 @@ class AddCartButtonInThumbnailWrap extends ProductLoopItemContent
         return $cartLink;
     }
 
+    public function addCartButtonPosition($itemClasses)
+    {
+        $itemClasses[] = sprintf(
+            'pos-%s',
+            apply_filters('jankx/woocommerce/add_cart/button', 'bottom-right')
+        );
+        return $itemClasses;
+    }
+
     public function contentStart()
     {
         remove_action('woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart', 10);
         add_action('jankx/post_layout/thumbnail/after', 'woocommerce_template_loop_add_to_cart', 10);
-        add_filter('woocommerce_loop_add_to_cart_link', [$this, 'renderCartButtonIcon'], 10, 2);
+
+        add_filter($this->getPostClassHook(), [$this, 'addCartButtonPosition'], 10);
+
+
+        add_action('woocommerce_loop_add_to_cart_link', [$this, 'renderCartButtonIcon'], 10, 2);
+        add_action('woocommerce_before_shop_loop_item_title', [$this, 'wrapProductInfoTag'], 25);
+        add_action('woocommerce_after_shop_loop_item', [$this, 'closeWrapProductInfoTag'], 45);
+
+
+        remove_action('woocommerce_before_shop_loop_item', 'woocommerce_template_loop_product_link_open', 10);
+        remove_action('woocommerce_after_shop_loop_item', 'woocommerce_template_loop_product_link_close', 5);
+
+        add_action('woocommerce_shop_loop_item_title', 'woocommerce_template_loop_product_link_open', 9);
+        add_action('woocommerce_shop_loop_item_title', 'woocommerce_template_loop_product_link_close', 11);
     }
 
 
     public function contentEnd()
     {
-        remove_filter('woocommerce_loop_add_to_cart_link', [$this, 'renderCartButtonIcon']);
+
+        add_action('woocommerce_before_shop_loop_item', 'woocommerce_template_loop_product_link_open', 10);
+        add_action('woocommerce_after_shop_loop_item', 'woocommerce_template_loop_product_link_close', 5);
+
+        remove_action('woocommerce_before_shop_loop_item_title', [$this, 'wrapProductInfoTag'], 25);
+        remove_action('woocommerce_after_shop_loop_item', [$this, 'closeWrapProductInfoTag'], 45);
+        remove_action('woocommerce_loop_add_to_cart_link', [$this, 'renderCartButtonIcon']);
+
+        remove_filter($this->getPostClassHook(), [$this, 'addCartButtonPosition'], 10);
+
         remove_action('jankx/post_layout/thumbnail/after', 'woocommerce_template_loop_add_to_cart', 10);
+
         add_action('woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart', 10);
     }
+
+
+    public function wrapProductInfoTag()
+    {
+        ?>
+        <div class="product-info">
+        <?php
+    }
+    public function closeWrapProductInfoTag()
+    {
+        ?>
+        </div>
+        <?php
+    }
 }
+
--- includes/WooCommerce.php
+++ includes/WooCommerce.php
@@ -18,7 +18,7 @@ use Jankx\WooCommerce\WooCommerceTemplate;
 class WooCommerce
 {
     const NAME = 'jankx-ecommerce';
-    const VERSION = '1.0.0.5';
+    const VERSION = '1.0.0.6';
 
     protected static $instance;
     protected static $supportPlugins;
