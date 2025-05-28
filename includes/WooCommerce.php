4ebb5556475c7a51123927d21c8824857efb555b
Puleeno Nguyen <puleeno@gmail.com>
2025-05-28 17:54:42 +0700

    upversion to 1.0.28

--- includes/WooCommerce.php
+++ includes/WooCommerce.php
@@ -18,7 +18,7 @@ use Jankx\WooCommerce\WooCommerceTemplate;
 class WooCommerce
 {
     const NAME = 'jankx-ecommerce';
-    const VERSION = '1.0.25';
+    const VERSION = '1.0.28';
 
     protected static $instance;
     protected static $singleProductLayouts;
