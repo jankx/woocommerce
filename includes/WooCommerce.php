fe6682a461b4137cb8ddf774e0abc5ce1f563969
Puleeno Nguyen <puleeno@gmail.com>
2025-05-31 20:00:19 +0700

    Update stylesheet for single product

--- includes/WooCommerce.php
+++ includes/WooCommerce.php
@@ -18,7 +18,7 @@ use Jankx\WooCommerce\WooCommerceTemplate;
 class WooCommerce
 {
     const NAME = 'jankx-ecommerce';
-    const VERSION = '1.0.32';
+    const VERSION = '1.0.33';
 
     protected static $instance;
     protected static $singleProductLayouts;
