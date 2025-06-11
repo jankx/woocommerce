a462482ed1abd17668ff235a61cd763cbb98b09d
Puleeno Nguyen <puleeno@gmail.com>
2025-06-11 23:08:02 +0700

    up woo package version

--- includes/WooCommerce.php
+++ includes/WooCommerce.php
@@ -18,7 +18,7 @@ use Jankx\WooCommerce\WooCommerceTemplate;
 class WooCommerce
 {
     const NAME = 'jankx-ecommerce';
-    const VERSION = '1.0.36';
+    const VERSION = '1.0.38';
 
     protected static $instance;
     protected static $singleProductLayouts;
