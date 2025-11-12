fdece75562bfd7d135cd758580b372830dca7b70
Puleeno Nguyen <puleeno@gmail.com>
2025-11-13 01:02:10 +0700

    support render inner blocks for WooCommerce

--- includes/PostLayout/WooCommerceContentGenerator.php
+++ includes/PostLayout/WooCommerceContentGenerator.php
@@ -2,8 +2,8 @@
 
 namespace Jankx\WooCommerce\PostLayout;
 
+use Jankx\Gutenberg\Blocks\PostLayoutTemplateBlock;
 use Jankx\Layouts\PostLayout\Generators\AbstractContentGenerator;
-use Jankx\Layouts\PostLayout\Generators\PostTemplateBlockGenerator;
 use WP_Query;
 
 /**
@@ -34,14 +34,6 @@ class WooCommerceContentGenerator extends AbstractContentGenerator
 
     protected function renderContent(WP_Query $query, array $options = []): string
     {
-        if (!empty($options['postTemplate']) && is_array($options['postTemplate'])) {
-            $templateGenerator = new PostTemplateBlockGenerator($options['postTemplate'], $options);
-            if ($layout = $this->getLayout()) {
-                $templateGenerator->setLayout($layout);
-            }
-            return $templateGenerator->generate($query, $options);
-        }
-
         if (!$this->isWooCommerceActive()) {
             return '<div class="woocommerce-error">' . __('WooCommerce is not active', 'jankx') . '</div>';
         }
@@ -50,118 +42,25 @@ class WooCommerceContentGenerator extends AbstractContentGenerator
             return '<div class="no-products">' . __('No products found.', 'woocommerce') . '</div>';
         }
 
-        global $wp_query, $product;
-        $original_query = $wp_query;
-        $original_product = isset($product) ? $product : null;
-        $wp_query = $query;
+        $options = $this->normalizeOptions($options);
+        $templateBlock = $options['postTemplate'] ?? null;
 
-        $columns = $options['columns'] ?? 3;
-        $showFeaturedImage = $options['showFeaturedImage'] ?? true;
-        $showTitle = $options['showTitle'] ?? true;
-        $showPrice = $options['showPrice'] ?? true;
-        $showAddToCart = $options['showAddToCart'] ?? true;
-        $showSaleBadge = $options['showSaleBadge'] ?? true;
-        $imageSize = $options['imageSize'] ?? 'woocommerce_thumbnail';
+        if (!is_array($templateBlock) || empty($templateBlock)) {
+            $templateBlock = $this->getDefaultTemplateBlock($options);
+        }
 
-        ob_start();
-        ?>
-        <div data-wp-interactive="woocommerce/product-collection" data-wp-context='{"notices":[]}' class="wp-block-woocommerce-product-collection is-layout-flow wp-block-product-collection-is-layout-flow">
-            <div data-wp-interactive="woocommerce/store-notices" class="wc-block-components-notices alignwide"></div>
-            <ul data-block-name="woocommerce/product-template" class="wc-block-product-template__responsive columns-<?php echo esc_attr($columns); ?> wc-block-product-template wp-block-woocommerce-product-template is-layout-flow wp-block-product-template-is-layout-flow">
-            <?php
-            while ($query->have_posts()) {
-                $query->the_post();
-                global $product;
-                wc_setup_product_data($GLOBALS['post']);
+        if (!is_array($templateBlock) || empty($templateBlock)) {
+            return '';
+        }
 
-                $product_id = $product->get_id();
-                $product_type = $product->get_type();
-                $product_classes = $this->getProductClasses($product);
-                $cart_quantity = $this->getCartQuantity($product_id);
-                $is_in_cart = $cart_quantity > 0;
-                ?>
-                <li class="<?php echo esc_attr($product_classes); ?>" data-wp-interactive="woocommerce/product-collection" data-wp-context="<?php echo esc_attr(json_encode(['productId' => $product_id])); ?>" data-wp-key="product-item-<?php echo esc_attr($product_id); ?>">
-                    <?php if ($showFeaturedImage): ?>
-                    <div data-block-name="woocommerce/product-image" data-image-sizing="thumbnail" data-is-descendent-of-query-loop="true" data-show-sale-badge="<?php echo $showSaleBadge ? 'true' : 'false'; ?>" class="wc-block-components-product-image wc-block-grid__product-image wp-block-woocommerce-product-image">
-                        <a href="<?php echo esc_url(get_permalink()); ?>" data-wp-on--click="woocommerce/product-collection::actions.viewProduct">
-                            <?php echo $product->get_image($imageSize, [
-                                'class' => 'attachment-woocommerce_thumbnail size-woocommerce_thumbnail',
-                                'style' => 'object-fit:cover;',
-                                'data-testid' => 'product-image',
-                                'data-image-id' => get_post_thumbnail_id(),
-                            ]); ?>
-                            <div class="wc-block-components-product-image__inner-container">
-                                <?php if ($showSaleBadge && $product->is_on_sale()): ?>
-                                <div data-block-name="woocommerce/product-sale-badge" class="wp-block-woocommerce-product-sale-badge">
-                                    <div class="wc-block-components-product-sale-badge alignright wc-block-components-product-sale-badge--align-right">
-                                        <span class="wc-block-components-product-sale-badge__text" aria-hidden="true"><?php echo esc_html__('Khuyến mại', 'woocommerce'); ?></span>
-                                        <span class="screen-reader-text"><?php echo esc_html__('Sản phẩm đang giảm giá', 'woocommerce'); ?></span>
-                                    </div>
-                                </div>
-                                <?php endif; ?>
-                            </div>
-                        </a>
-                    </div>
-                    <?php endif; ?>
+        $options['postTemplate'] = $templateBlock;
 
-                    <?php if ($showTitle): ?>
-                    <h2 class="has-text-align-center wp-block-post-title has-medium-font-size" style="line-height:1.4;margin-bottom:0.75rem;margin-top:0;">
-                        <a data-wp-on--click="woocommerce/product-collection::actions.viewProduct" href="<?php echo esc_url(get_permalink()); ?>" target="_self"><?php echo esc_html(get_the_title()); ?></a>
-                    </h2>
-                    <?php endif; ?>
-
-                    <?php if ($showPrice): ?>
-                    <div data-block-name="woocommerce/product-price" data-font-size="small" data-is-descendent-of-query-loop="true" data-text-align="center" class="wp-block-woocommerce-product-price">
-                        <div class="wc-block-components-product-price wc-block-grid__product-price has-text-align-center has-font-size has-small-font-size">
-                            <?php echo $product->get_price_html(); ?>
-                        </div>
-                    </div>
-                    <?php endif; ?>
-
-                    <?php if ($showAddToCart): ?>
-                    <?php $notice_id = 'wc-add-to-cart-notice-' . $product_id . '-' . wp_unique_id(); ?>
-                    <div data-block-name="woocommerce/product-button" data-font-size="small" data-is-descendent-of-query-loop="true" data-text-align="center" class="wp-block-button wc-block-components-product-button align-center wp-block-woocommerce-product-button has-small-font-size" data-wp-interactive="woocommerce/product-button" data-wp-init="actions.refreshCartItems" data-wp-context="<?php echo esc_attr(json_encode([
-                        'quantityToAdd' => 1,
-                        'productId' => $product_id,
-                        'productType' => $product_type,
-                        'addToCartText' => __('Thêm vào giỏ hàng', 'woocommerce'),
-                        'tempQuantity' => $cart_quantity,
-                        'animationStatus' => 'IDLE',
-                        'inTheCartText' => sprintf(__('Có ### trong giỏ hàng', 'woocommerce')),
-                        'noticeId' => $notice_id,
-                        'hasPressedButton' => false,
-                    ])); ?>">
-                        <?php if ($product->is_purchasable() && $product->is_in_stock()): ?>
-                        <button class="wp-block-button__link wp-element-button wc-block-components-product-button__button add_to_cart_button ajax_add_to_cart product_type_<?php echo esc_attr($product_type); ?> has-font-size has-small-font-size" type="button" data-product_id="<?php echo esc_attr($product_id); ?>" data-product_sku="<?php echo esc_attr($product->get_sku()); ?>" aria-label="<?php echo esc_attr(sprintf(__('Thêm vào giỏ hàng: "%s"', 'woocommerce'), get_the_title())); ?>" data-wp-on--click="actions.addCartItem">
-                            <span data-wp-class--wc-block-slide-in="state.slideInAnimation" data-wp-class--wc-block-slide-out="state.slideOutAnimation" data-wp-on--animationend="actions.handleAnimationEnd" data-wp-watch="callbacks.startAnimation" data-wp-run="callbacks.syncTempQuantityOnLoad" data-wp-on--click="actions.handlePressedState">
-                                <?php echo $is_in_cart ? sprintf(__('Có %d trong giỏ hàng', 'woocommerce'), $cart_quantity) : __('Thêm vào giỏ hàng', 'woocommerce'); ?>
-                            </span>
-                        </button>
-                        <?php else: ?>
-                        <a class="wp-block-button__link wp-element-button wc-block-components-product-button__button product_type_<?php echo esc_attr($product_type); ?> has-font-size has-small-font-size" href="<?php echo esc_url(get_permalink()); ?>" rel="nofollow" data-product_id="<?php echo esc_attr($product_id); ?>" data-product_sku="<?php echo esc_attr($product->get_sku()); ?>" aria-label="<?php echo esc_attr(sprintf(__('Đọc thêm về "%s"', 'woocommerce'), get_the_title())); ?>" data-wp-on--click="woocommerce/product-collection::actions.viewProduct">
-                            <span><?php echo esc_html__('Đọc tiếp', 'woocommerce'); ?></span>
-                        </a>
-                        <?php endif; ?>
-                        <span hidden data-wp-bind--hidden="!state.displayViewCart">
-                            <a href="<?php echo esc_url(wc_get_cart_url()); ?>" class="added_to_cart wc_forward" title="<?php echo esc_attr__('Xem giỏ hàng', 'woocommerce'); ?>">
-                                <?php echo esc_html__('Xem giỏ hàng', 'woocommerce'); ?>
-                            </a>
-                        </span>
-                    </div>
-                    <?php endif; ?>
-                </li>
-                <?php
-            }
-            ?>
-            </ul>
-        </div>
-        <?php
-
-        wp_reset_postdata();
-        $wp_query = $original_query;
-        $product = $original_product;
-
-        return ob_get_clean();
+        return PostLayoutTemplateBlock::renderTemplateWithQuery(
+            $templateBlock,
+            $query,
+            $options,
+            $this->getLayout()
+        );
     }
 
     protected function renderPreviewContent(array $options = []): array
@@ -222,61 +121,6 @@ class WooCommerceContentGenerator extends AbstractContentGenerator
         return true;
     }
 
-    protected function getProductClasses($product): string
-    {
-        $classes = [
-            'wc-block-product',
-            'post-' . $product->get_id(),
-            'product',
-            'type-product',
-            'status-' . $product->get_status(),
-        ];
-
-        if ($product->get_image_id()) {
-            $classes[] = 'has-post-thumbnail';
-        }
-
-        $categories = wp_get_post_terms($product->get_id(), 'product_cat', ['fields' => 'slugs']);
-        if (!empty($categories)) {
-            $classes = array_merge($classes, array_map(function ($cat) {
-                return 'product_cat-' . $cat;
-            }, $categories));
-        }
-
-        $classes[] = $product->is_in_stock() ? 'instock' : 'outofstock';
-
-        if ($product->is_shipping_taxable()) {
-            $classes[] = 'shipping-taxable';
-        }
-
-        if ($product->is_purchasable()) {
-            $classes[] = 'purchasable';
-        }
-
-        if ($product->is_on_sale()) {
-            $classes[] = 'sale';
-        }
-
-        $classes[] = 'product-type-' . $product->get_type();
-
-        return implode(' ', $classes);
-    }
-
-    protected function getCartQuantity(int $product_id): int
-    {
-        if (!function_exists('WC') || !WC()->cart) {
-            return 0;
-        }
-
-        foreach (WC()->cart->get_cart() as $cart_item) {
-            if ($cart_item['product_id'] == $product_id) {
-                return $cart_item['quantity'];
-            }
-        }
-
-        return 0;
-    }
-
     protected function generatePreviewItems(array $options = []): array
     {
         $count = min($options['postsPerPage'] ?? 6, 6);
@@ -294,6 +138,85 @@ class WooCommerceContentGenerator extends AbstractContentGenerator
         }
 
         return $items;
+    }
+
+    protected function normalizeOptions(array $options): array
+    {
+        if (empty($options['itemsWrapperClass'])) {
+            $options['itemsWrapperClass'] = 'wc-block-product-template';
+        }
+
+        if (empty($options['itemClass'])) {
+            $options['itemClass'] = 'wc-block-product';
+        }
+
+        return $options;
+    }
+
+    protected function getDefaultTemplateBlock(array $options): array
+    {
+        $content = $this->buildDefaultTemplateContent($options);
+
+        if (empty($content)) {
+            return [];
+        }
+
+        $blocks = parse_blocks($content);
+
+        if (empty($blocks)) {
+            return [];
+        }
+
+        return $blocks[0];
+    }
+
+    protected function buildDefaultTemplateContent(array $options): string
+    {
+        $blocks = [];
+
+        $showFeaturedImage = $options['showFeaturedImage'] ?? true;
+        $showSaleBadge = $options['showSaleBadge'] ?? true;
+        $showTitle = $options['showTitle'] ?? true;
+        $showPrice = $options['showPrice'] ?? true;
+        $showRating = $options['showRating'] ?? false;
+        $showAddToCart = $options['showAddToCart'] ?? true;
+
+        if ($showFeaturedImage) {
+            $blocks[] = sprintf(
+                '<!-- wp:woocommerce/product-image {"align":"center","showSaleBadge":%s} /-->',
+                $showSaleBadge ? 'true' : 'false'
+            );
+        }
+
+        if ($showTitle) {
+            $blocks[] = '<!-- wp:woocommerce/product-title {"textAlign":"center","level":3} /-->';
+        }
+
+        if ($showRating) {
+            $blocks[] = '<!-- wp:woocommerce/product-rating {"textAlign":"center"} /-->';
+        }
+
+        if ($showPrice) {
+            $blocks[] = '<!-- wp:woocommerce/product-price {"textAlign":"center","fontSize":"small"} /-->';
+        }
+
+        if ($showAddToCart) {
+            $blocks[] = '<!-- wp:woocommerce/product-button {"textAlign":"center","fontSize":"small"} /-->';
+        }
+
+        if (empty($blocks)) {
+            $blocks[] = '<!-- wp:woocommerce/product-title {"textAlign":"center"} /-->';
+        }
+
+        $wrapperAttrs = [
+            'className' => 'wc-block-product-template wc-block-product-template--default',
+        ];
+
+        return sprintf(
+            '<!-- wp:jankx/post-layout-template %s -->%s<!-- /wp:jankx/post-layout-template -->',
+            wp_json_encode($wrapperAttrs),
+            implode('', $blocks)
+        );
     }
 }
 
